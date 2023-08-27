1bc19a8221c03c7008338ba368a50178bfe9931c
Abhishek Tiwati <abhishek748932@gmail.com>
2023-08-27 13:09:27 +0530

    Day 1 Done

---
+++ app/src/main/java/com/my/myportfolioapp/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.my.myportfolioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
